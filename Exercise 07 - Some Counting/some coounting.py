766af2d40fec2eda60fac81bf366476887e83624
zainab432005 <zezemou432005@gmail.com>
2026-01-12 10:41:54 +0400

    Add files via upload

---
+++ Exercise 07 - Some Counting/some coounting.py
@@ -0,0 +1,28 @@
+# 1. Count up from 0 to 50
+print("Count up from 0 to 50:")
+for i in range(0, 51, 1):
+    print(i)
+print("\n")
+
+# 2. Count down from 50 to 0
+print("Count down from 50 to 0:")
+for i in range(50, -1, -1):
+    print(i)
+print("\n")
+
+# 3. Count up from 30 to 50
+print("Count up from 30 to 50:")
+for i in range(30, 51, 1):
+    print(i)
+print("\n")
+
+# 4. Count down from 50 to 10 in decrements of 2
+print("Count down from 50 to 10 (step -2):")
+for i in range(50, 9, -2):
+    print(i)
+print("\n")
+
+# 5. Count up from 100 to 200 in increments of 5
+print("Count up from 100 to 200 (step 5):")
+for i in range(100, 201, 5):
+    print(i)
